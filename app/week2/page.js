6a3d3c67ee46ddde972143522b6b1f134ce9dd9c
NathanCCH <nathan.cch@outlook.com>
2024-01-16 12:57:58 -0700

    Add week2 demo

---
+++ app/week2/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <main>
+            <h1 className="text-4xl">Week2 Demo</h1>
+            <p>This is a paragraph.</p>
+        </main>
+    );
+}
