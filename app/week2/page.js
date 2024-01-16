48927d6e5836ab0b36973d1c7a7adb217a6c9c01
NathanCCH <nathan.cch@outlook.com>
2024-01-16 13:24:40 -0700

    Add something into heading

--- app/week2/page.js
+++ app/week2/page.js
@@ -1,11 +1,18 @@
 import Heading from "./heading";
 
 export default function Page() {
+
+    let a = 3;
+    let b = 2;
+    let c = a + b;
+
     return (
         <main>
             <Heading />
             <h1 className="text-4xl">Week2 Demo</h1>
             <p>This is a paragraph.</p>
+            <p>The value of a is {a}.</p>
+            <p>The sum of a and b = {c}.</p>
         </main>
     );
 }
